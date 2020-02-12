f7c5af4bb42c217217f34b5c093cec9e46bdfe3b
Julien Savoie <juliensavoie7@gmail.com>
2020-02-11 21:02:52 -0500

    Style Slider

---
+++ js/configs/configSlider.js
@@ -0,0 +1,31 @@
+const configSlider = {
+    largeur : "400px",
+    hauteur : "300px",
+    contenu : [
+        {
+            titre : "Image #1",
+            sousTitre : "Une belle image",
+            src : "https://pngimage.net/wp-content/uploads/2018/06/smile.png-.png"
+        },
+        {
+            titre : "Image #2",
+            sousTitre : "Une belle image",
+            src : "https://pngimage.net/wp-content/uploads/2018/06/smile.png-.png"
+        },
+        {
+            titre : "Image #3",
+            sousTitre : "Une belle image",
+            src : "https://pngimage.net/wp-content/uploads/2018/06/smile.png-.png"
+        },
+        {
+            titre : "Image #4",
+            sousTitre : "Une belle image",
+            src : "https://pngimage.net/wp-content/uploads/2018/06/smile.png-.png"
+        },
+        {
+            titre : "Image #5",
+            sousTitre : "Une belle image",
+            src : "https://pngimage.net/wp-content/uploads/2018/06/smile.png-.png"
+        }
+    ]
+}
