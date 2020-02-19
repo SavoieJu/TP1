92d0b63bb5b122c98099b1659d8d689c45657587
Julien Savoie <juliensavoie7@gmail.com>
2020-02-18 23:07:21 -0500

    Ajout date et heure dans header

--- js/main.js
+++ js/main.js
@@ -6,4 +6,11 @@ sideScrollStart.startMoving();
 
 let waves = new Waves(300, 30);
 waves.prepareBoxForWave();
- 
+
+var date = new Date();
+chargerTemps(date);
+
+function chargerTemps(date) {
+    const options = {weekday: 'long', year: 'numeric', month: 'long', day: 'numeric', hour: 'numeric'};
+    document.querySelector("header > span").innerHTML = "On est le " + date.toLocaleDateString('fr-FR', options) + "";
+}
